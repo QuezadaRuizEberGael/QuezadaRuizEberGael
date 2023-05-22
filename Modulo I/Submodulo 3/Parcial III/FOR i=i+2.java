1a554b48704bde3ed149958bc31509b4b55d1813
QuezadaRuizEberGael <126712230+QuezadaRuizEberGael@users.noreply.github.com>
2023-05-22 13:35:06 -0600

    Add files via upload

---
+++ Modulo I/Submodulo 3/Parcial III/FOR i=i+2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class clase
+  {    
+    public static void main(String[] args)
+     {
+      //declarar variables e introducir informacion
+      int a[] = {-3,1,-25,5,300};
+      //Procesar la informacion es decir, organizar la tabla de multiplicacion
+      for(int i= 0; i < a.length; i=i+2)
+       System.out.println(a[i]);
+     }
+  }
---
+++ Modulo I/Submodulo 3/Parcial III/WHILEi=i+2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class clase
+  {    
+    public static void main(String[] args)
+     {
+      //declarar variables e introducir informacion
+      int a[] = {-3,1,-25,5,300};
+      int i = 0;
+      //Procesar la informacion es decir, organizar la tabla de multiplicacion
+      while(i<a.length)
+       {
+        System.out.println(a[i]);
+        i=i+2;
+       }
+     }
+  }
